0dfd8a38b3dedc6ecaab8f8f24fcff2dd0029042
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2024-03-26 00:46:32 +0800

    Worked on post slice, counter slice, and components that use the slices

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Counter from "./features/counter/Counter";
+import PostList from "./features/posts/PostList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
   return (
     <>
-      <Counter />
+      <AddPostForm />
+      <PostList />
     </>
   );
 }
